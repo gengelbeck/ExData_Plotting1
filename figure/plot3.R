34e917e412428302cc23453356ae15bc784f735b
George Engelbeck <gengelbeck@gmail.com>
2014-06-08 11:08:06 -0700

    Updated plot4.R and plot4.R to adjust margins.
    Signed-off-by: George Engelbeck <gengelbeck@gmail.com>

--- figure/plot3.R
+++ figure/plot3.R
@@ -8,19 +8,25 @@ load("ds.RData")
 ds$DateTime <- strptime(paste(ds$Date, ds$Time), "%d/%m/%Y %H:%M:%S")
 
 # Set the margins
-par(mar=c(4,4,3,1))
+par(mar=(c(5, 4, 4, 2) + 0.1))
 
 # Plot the power usage by day of the week (default)
 plot(ds$DateTime,ds$Sub_metering_1,
-    type="l",
-    cex.lab = 0.75,
-    cex.axis = 0.75,
-    ylab = "Energy sub metering", 
-    xlab = "",
-    col="black")
+     type="l",
+     cex.lab = 0.75,
+     cex.axis = 0.75,
+     ylab = "Energy sub metering", 
+     xlab = "",
+     col="black")
 lines(ds$DateTime,ds$Sub_metering_2,col="red")
 lines(ds$DateTime,ds$Sub_metering_3,col="blue")
-legend("topright", lty = 1, cex=0.7, col=c("black", "red", "blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+legend("topright", 
+       inset = c(0.08, 0.00, 0.00, 0.00),
+       lty = 1, 
+       cex=0.6,
+       col=c("black", "red", "blue"),
+       bty = "n",
+       legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
 # Make a copy of the plot as a PNG and stave it to a file
 dev.copy(png, "plot3.png",width = 480, height = 480, units = "px")
